1a7ae9541b74be8de2b0ce15ee0773bd05d05a46
msy7822-ux <msy780022@gmail.com>
2023-06-03 22:51:06 +0900

    get userId on Route Handler

--- src/app/api/supabase/create-user-profile/route.ts
+++ src/app/api/supabase/create-user-profile/route.ts
@@ -1,12 +1,6 @@
-// import { auth } from "@clerk/nextjs";
-// import { getAuth } from "@clerk/nextjs/server";
-
 export async function POST(request: Request) {
-  // console.log(request.headers);
   const { data } = await request.json();
-  console.log(data.id);
-  console.log(data["id"]);
-  // console.log(data["userId"]);
+  const userId = data.id;
 
   return new Response(`created user id}`);
 }
