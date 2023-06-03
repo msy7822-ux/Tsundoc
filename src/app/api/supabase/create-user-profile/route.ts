bb80a177ebb40dc4c32028c90334f737f4e8ac28
msy7822-ux <msy780022@gmail.com>
2023-06-03 19:59:48 +0900

    modify webhook Route Handler

--- src/app/api/supabase/create-user-profile/route.ts
+++ src/app/api/supabase/create-user-profile/route.ts
@@ -2,9 +2,8 @@
 // import { getAuth } from "@clerk/nextjs/server";
 
 export async function POST(request: Request) {
-  console.log(request);
+  console.log(request.headers);
   console.log(await request.json());
-  console.log(request.body);
 
   // console.log("GET userId", userId);
   return new Response(`created user id}`);
