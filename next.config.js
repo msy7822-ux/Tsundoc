3993222e4fc1b0217b81b0eb518c584190ef8ab7
msy7822-ux <msy780022@gmail.com>
2023-06-04 15:03:45 +0900

    setup sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
+
 const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   dest: "public",
@@ -7,6 +8,40 @@ const withPWA = require("next-pwa")({
   skipWaiting: false,
   runtimeCaching,
 });
+
+const { withSentryConfig } = require("@sentry/nextjs");
+const sentryConfig = withSentryConfig(
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+
+    org: "ms-ee",
+    project: "tsundoc",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
+
 const nextConfig = {
   experimental: {
     appDir: true,
@@ -16,6 +51,7 @@ const nextConfig = {
     domains: ["images.clerk.dev"],
   },
   ...withPWA({}),
+  ...sentryConfig,
 };
 
 module.exports = nextConfig;
