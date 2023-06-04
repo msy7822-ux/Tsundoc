69065720b7e1232a26847a6d6388617355f4d778
msy7822-ux <msy780022@gmail.com>
2023-06-04 16:14:08 +0900

    remove global toast container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import { Header } from "./_components/common/header/header";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ToastContainer } from "react-toastify";
 import "./globals.css";
-import "react-toastify/dist/ReactToastify.css";
 
 export default async function RootLayout({
   children,
@@ -17,7 +15,6 @@ export default async function RootLayout({
           <link rel="manifest" href="/manifest.json" />
         </head>
         <body>
-          <ToastContainer />
           <Header></Header>
           <div className="p-3 sm:p-5 sm:px-10 w-full">{children}</div>
         </body>
