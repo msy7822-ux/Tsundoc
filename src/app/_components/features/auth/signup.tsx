47ed8147ba43ef1ad64a20fdc6adf03f9760caed
masahiro fukatsu <msy@masahironoMacBook-Pro.local>
2023-06-20 23:49:13 +0900

    modify redirect URL

--- src/app/_components/features/auth/signup.tsx
+++ src/app/_components/features/auth/signup.tsx
@@ -12,6 +12,7 @@ export function SignupComponent() {
     try {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "github",
+        options: { redirectTo: "https://tsun-doc.com/" },
       });
       // console.log("on click");
       // const data = await signup();
