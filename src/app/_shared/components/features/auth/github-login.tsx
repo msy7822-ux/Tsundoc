5d63fe820a14d6eefea1147f29be9e34cdfff653
masahiro fukatsu <msy@masahironoMacBook-Pro.local>
2023-07-02 13:43:15 +0900

    add router refresh proceed

--- src/app/_shared/components/features/auth/github-login.tsx
+++ src/app/_shared/components/features/auth/github-login.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { login } from "@/lib/auth/client/login";
+import { useRouter } from "next/navigation";
 import { AiOutlineGithub } from "react-icons/ai";
 import { AuthButton } from "./auth-button";
 
 export function GitHubLoginComponent() {
+  const router = useRouter();
   const handleOnClick = async () => {
     try {
       await login("github");
+      router.refresh();
     } catch (error) {
       console.error(error);
     }
--- src/app/_shared/components/features/auth/google-login.tsx
+++ src/app/_shared/components/features/auth/google-login.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { login } from "@/lib/auth/client/login";
+import { useRouter } from "next/navigation";
 import { FcGoogle } from "react-icons/fc";
 import { AuthButton } from "./auth-button";
 
 export function GoogleLoginComponent() {
+  const router = useRouter();
+
   const handleOnClick = async () => {
     try {
       await login("google");
+      router.refresh();
     } catch (error) {
       console.error(error);
     }
